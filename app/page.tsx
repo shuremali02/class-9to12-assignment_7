236b446c32c22a5d60b0222854260936b85d7cbe
shuremali02 <shuremsyed41@gmail.com>
2024-12-29 15:33:39 -0800

    mobile screen minor issue resolve

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
        border border-b border-gray-700 pb-2 text-2xl md:text-5xl mb-6 animate-bounce">Welcome to My Assignment</h1>
         <p className="text-2xl font-bold font-serif animate-pulse">Check in </p>
       </div>
-      <div className="flex flex-col md:flex-row gap-4">
+      <div className="md:flex flex-col hidden md:flex-row gap-4">
        <Button  button="Server Side Data Fetching" href="/Server-side"/>
        <Button button=" Client Side Data Fetching" href="/Client-side"/>
         
